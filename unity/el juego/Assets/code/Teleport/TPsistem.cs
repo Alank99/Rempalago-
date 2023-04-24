fb46b4b6fe488da8e0e7ac0db64b5e54dd242a46
Alank99 <93668758+Alank99@users.noreply.github.com>
2023-04-24 12:28:24 -0600

    fondo universal

--- unity/el juego/Assets/code/Teleport/TPsistem.cs
+++ unity/el juego/Assets/code/Teleport/TPsistem.cs
@@ -1,24 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class TPsistem : MonoBehaviour
-{
-    //jugador
-    public GameObject player;
-    [SerializeField] 
-    GameObject
-    id;
+public class TeleportScript : MonoBehaviour {
+    //La posición a la que se teleportará el jugador
+    public Transform teleportPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //El objeto al que se teleportará
+    public GameObject targetObject;
+
+    //La función que se ejecutará cuando el botón se presione
+    public void TeleportToTarget() {
+        //Obtener el objeto jugador
+        GameObject player = GameObject.FindWithTag("Player");
+
+        //Si el jugador existe y el objeto destino existe
+        if (player != null && targetObject != null) {
+            //Teleportar al jugador al objeto destino
+            player.transform.position = targetObject.transform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //La función que se ejecutará cuando se seleccione un objeto destino
+    public void SetTargetObject(GameObject newTarget) {
+        //Asignar el objeto destino seleccionado a la variable targetObject
+        targetObject = newTarget;
     }
 }
