d87c6667a8be0f0bb733893c2716ae5e7a93d5a4
Oswaldo Mendizabal <ilhuicatzi124mendizabal@gmail.com>
2023-04-28 17:44:45 -0600

    adds coin counter to UI

---
+++ unity/el juego/Assets/code/UI/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    static GameController current;
+    private int coins;
+
+    public static void addCoin()
+    {
+        current.coins++;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
