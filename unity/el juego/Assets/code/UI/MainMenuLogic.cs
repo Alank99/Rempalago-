bacc82ff39e59374afbcb74c5ed268fdc8824f10
Oswaldo Mendizabal <ilhuicatzi124mendizabal@gmail.com>
2023-05-31 13:45:28 -0600

    creates options menu and links every game scene

--- unity/el juego/Assets/code/UI/MainMenuLogic.cs
+++ unity/el juego/Assets/code/UI/MainMenuLogic.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuLogic : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenu = GameObject.Find("MainMenuCanvas");
+        optionsMenu = GameObject.Find("OptionsCanvas");
+
+        mainMenu.GetComponent<Canvas>().enabled = true;
+        optionsMenu.GetComponent<Canvas>().enabled = false; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartButton()
     {
-        
+       mainMenu.GetComponent<Canvas>().enabled = false; 
+       SceneManager.LoadScene("Login");
+    }
+
+    public void OptionsButton()
+    {
+        mainMenu.GetComponent<Canvas>().enabled = false; 
+        optionsMenu.GetComponent<Canvas>().enabled = true; 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Bye bye!");
+    }
+
+    public void ReturnToMainMenuButton()
+    {
+        mainMenu.GetComponent<Canvas>().enabled = true;
+        optionsMenu.GetComponent<Canvas>().enabled = false;
     }
 }
