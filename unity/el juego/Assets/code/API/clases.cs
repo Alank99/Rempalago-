f5ac378b014b6ae44eba53a65635c4deace87011
Mario Frias <a01782559@tec.mx>
2023-06-12 15:12:15 -0600

    Droptables with sql

--- unity/el juego/Assets/code/API/clases.cs
+++ unity/el juego/Assets/code/API/clases.cs
@@ -68,6 +68,14 @@ public class checkpoint
 }
 
 [System.Serializable]
+public class loot_db
+{
+    public string name;
+    public int modifier;
+    public int probability;
+}
+
+[System.Serializable]
 public class playthroughList
 {
     public List<playthrough> list;
@@ -89,4 +97,10 @@ public class weaponList
 public class checkpointList
 {
     public List<checkpoint> list;
+}
+
+[System.Serializable]
+public class loot_dbList
+{
+    public List<loot_db> list;
 }
