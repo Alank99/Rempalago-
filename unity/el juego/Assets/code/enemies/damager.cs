641dde98f35002ea9ff1db4ca29d146f8c1e8dfa
Alex-Barr0n <a01783126@tec.mx>
2023-06-13 20:04:44 -0600

    Hands almost done, bullets don't deal damage

--- unity/el juego/Assets/code/enemies/damager.cs
+++ unity/el juego/Assets/code/enemies/damager.cs
@@ -7,7 +7,15 @@ public class damager : MonoBehaviour
 
     public int damage;
 
-     private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            HealthManager.healthSingleton.receiveDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
