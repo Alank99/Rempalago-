6d6f034093341093dd1307abb78428d77c4a255e
Alex-Barr0n <a01783126@tec.mx>
2023-06-14 23:45:31 -0600

    working on destroying head

--- unity/el juego/Assets/code/enemies/charroHead.cs
+++ unity/el juego/Assets/code/enemies/charroHead.cs
@@ -6,6 +6,7 @@ public class charroHead : MonoBehaviour
 {
     public GameObject player;
     public Rigidbody2D rb;
+    public charroHand charro_hand;
     public float speed;
 
     public void Update() {
@@ -22,5 +23,13 @@ public class charroHead : MonoBehaviour
     public void Start() {
         player = GameObject.FindWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void CheckHand()
+    {
+        if (charro_hand.salud <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
